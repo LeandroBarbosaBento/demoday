b9e7c9f4166c4ee94f3620cb8937224b06d5e957
leandro <leandro@pop-os.localdomain>
2024-04-01 13:03:39 -0300

    create default layout with menus

---
+++ src/components/layout/LayoutHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="header px-8">
+      <div class="header__content">
+        <div class="text-white app-font-weight-semibold app-font-size-xl">
+          {{ route.meta.pageTitle }}
+        </div>
+      </div>
+    </header>
+</template>
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<style scoped lang="scss">
+.header {
+  width: 100%;
+  height: 100%;
+  max-height: 90px;
+  display: flex;
+  align-items: center;
+
+  background: rgb(var(--v-theme-red-ufba));
+
+  &__content {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
