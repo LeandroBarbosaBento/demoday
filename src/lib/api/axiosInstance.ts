b98d317dde303c2617b013e1e7a78cb66f89e8ff
leandro <leandro@pop-os.localdomain>
2024-05-07 21:31:23 -0300

    config axios

---
+++ src/lib/api/axiosInstance.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosRequestConfig, AxiosRequestHeaders } from 'axios'
+
+interface AdaptAxiosRequestConfig extends AxiosRequestConfig {
+  headers: AxiosRequestHeaders
+}
+
+const axiosInstance = axios.create({
+  baseURL: 'https://demoday-api.onrender.com/',
+})
+
+const requestHandler = (requestConfig: AdaptAxiosRequestConfig) => {
+  try {
+    const token = ''
+    if (token) requestConfig.headers.Authorization = `bearer ${token}`;
+  } catch (e) {}
+  return requestConfig
+}
+
+axiosInstance.interceptors.request.use((requestConfig) => requestHandler(requestConfig))
+
+export default axiosInstance
