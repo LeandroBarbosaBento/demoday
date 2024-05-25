d5983afc1c08986b5b7ebf8834328ba70c3fcde1
leandro <leandro@pop-os.localdomain>
2024-05-24 23:17:10 -0300

    feat: integração da tela de usuários

--- src/pages/admin/users/index.vue
+++ src/pages/admin/users/index.vue
@@ -16,79 +16,80 @@
               </v-btn>
           </template>
           
-          <template v-slot:item.profile="{ item }">
+          <template v-slot:item.type="{ item }">
               <v-btn
                   class="py-4 cursor-default"
                   block
-                  :color="item.profile === 'professor' ? 'green-ufba' : 'blue-ufba'" 
+                  :color="userType[item.type].color" 
                   rounded="lg"
                   size="small"
                   flat
               >
-                  <span class="text-white font-weight-bold"> {{item.profile }} </span> 
+                  <span class="text-white font-weight-bold"> 
+                    {{userType[item.type].text }} 
+                  </span> 
               </v-btn>
           </template>
       </v-data-table>
   </div>
 
+  <Loader v-if="isLoading" />
+
 </template>
 <script setup lang="ts">
 import type { InternalDataTableHeader } from 'vuetify/lib/components/VDataTable/VDataTable'
+import axiosInstance from '@/api/axiosInstance';
+
 definePageMeta({
   layout: 'default-layout',
   pageTitle: 'Lista de usuários ',
   activeNavLink: 'users'
 })
 
-const items = [
-  {
-    id: 1,
-    name: 'Estudante da Silva Souza',
-    email: 'teste@gmail.com',
-    profile: 'professor'
-  },
-  {
-    id: 1,
-    name: 'Estudante da Silva Souza',
-    email: 'teste@gmail.com',
-    profile: 'aluno'
-  },
-  {
-    id: 1,
-    name: 'Estudante da Silva Souza',
-    email: 'teste@gmail.com',
-    profile: 'professor'
-  },
-  {
-    id: 1,
-    name: 'Estudante da Silva Souza',
-    email: 'teste@gmail.com',
-    profile: 'aluno'
-  },{
-    id: 1,
-    name: 'Estudante da Silva Souza',
-    email: 'teste@gmail.com',
-    profile: 'professor'
-  },
-  {
-    id: 1,
-    name: 'Estudante da Silva Souza',
-    email: 'teste@gmail.com',
-    profile: 'aluno'
-  }
-]
+interface User {
+  id: string
+  cpf: string
+  email: string
+  status: string
+  type: 'STUDENT' | 'ADMIN' | 'PROFESSOR'
+  university: string
+}
+
+const items = ref<User []>([]);
+const isLoading = ref(false);
+
+const userType = {
+  'STUDENT': {color: 'blue-ufba', text: 'estudante'},
+  'ADMIN': {color: 'red-ufba', text: 'administrador'},
+  'PROFESSOR': {color: 'green-ufba', text: 'professor'},
+}
 
 const headers = ref<InternalDataTableHeader>([
-  {title: 'id', key: 'id'},
   {title: 'Nome', key: 'name'},
   {title: 'Email', key:'email'},
-  {title: 'Perfil', key: 'profile'},
+  {title: 'Perfil', key: 'type'},
   {title: 'Ação', key: 'action'}
 ]);
 
 function editUser(user) {
   console.log(user)
 }
+
+async function listUsers(){
+  try {
+    isLoading.value = true;
+    const { data } = await axiosInstance.get('/user/getusers');
+    items.value = data;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(() => {
+  listUsers();
+});
 </script>
 
 <style>
