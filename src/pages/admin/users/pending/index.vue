21c11621a2b7993f52cca98315740d204290fff1
leandro <leandro@pop-os.localdomain>
2024-05-24 23:02:05 -0300

    feat: tela de usuários pendentes

--- src/pages/admin/users/pending/index.vue
+++ src/pages/admin/users/pending/index.vue
@@ -1,67 +1,86 @@
 <template>
-    <div class="px-8 py-8">
-        Lista de usuários pendentes
-        <v-data-table class="px-4 py-4 elevation-1 rounded-lg"
-            :headers="headers"
-            :items="items"
-        >
-            <template v-slot:item.action="{ item }">
-                <v-btn 
-                    variant="outlined" 
-                    color="blue-ufba" 
-                    rounded="lg"
-                >
-                    EDITAR
-                </v-btn>
-            </template>
-        </v-data-table>
-    </div>
+  <div class="px-8 py-8">
+      <v-data-table class="px-4 py-4 elevation-1 rounded-lg"
+          :headers="headers"
+          :items="items"
+      >
+          <template v-slot:item.action="{ item }">
+              <v-btn 
+                  class="py-5"
+                  variant="outlined" 
+                  block
+                  color="green-ufba" 
+                  rounded="lg"
+                  @click="aproveUser(item)"
+              >
+                  
+                  <span class="font-weight-bold"> 
+                    Aprovar cadastro
+                  </span>
+              </v-btn>
+          </template>
+          
+          <template v-slot:item.profile="{ item }">
+              <v-btn
+                  class="py-4 cursor-default"
+                  block
+                  color="blue-ufba" 
+                  rounded="lg"
+                  size="small"
+                  flat
+              >
+                  <span class="text-white font-weight-bold"> {{item.profile }} </span> 
+              </v-btn>
+          </template>
+      </v-data-table>
+  </div>
 
 </template>
 <script setup lang="ts">
 import type { InternalDataTableHeader } from 'vuetify/lib/components/VDataTable/VDataTable'
 definePageMeta({
   layout: 'default-layout',
-  pageTitle: 'Lista de usuários pendentes',
+  pageTitle: 'Lista de usuários pendentes ',
   activeNavLink: 'pending'
 })
 
 const items = [
-    {
-      id: 1,
-      name: 'Estudante da Silva Souza',
-      email: 'teste@gmail.com',
-      profile: 'professor'
-    },
-    {
-      id: 1,
-      name: 'Estudante da Silva Souza',
-      email: 'teste@gmail.com',
-      profile: 'aluno'
-    },
-    {
-      id: 1,
-      name: 'Estudante da Silva Souza',
-      email: 'teste@gmail.com',
-      profile: 'professor'
-    },
-    {
-      id: 1,
-      name: 'Estudante da Silva Souza',
-      email: 'teste@gmail.com',
-      profile: 'aluno'
-    },{
-      id: 1,
-      name: 'Estudante da Silva Souza',
-      email: 'teste@gmail.com',
-      profile: 'professor'
-    },
-    {
-      id: 1,
-      name: 'Estudante da Silva Souza',
-      email: 'teste@gmail.com',
-      profile: 'aluno'
-    }
+  {
+    id: 1,
+    name: 'Estudante da Silva Souza',
+    email: 'teste@gmail.com',
+    profile: 'professor'
+  },
+  {
+    id: 1,
+    name: 'Estudante da Silva Souza',
+    email: 'teste@gmail.com',
+    profile: 'professor'
+  },
+  {
+    id: 1,
+    name: 'Estudante da Silva Souza',
+    email: 'teste@gmail.com',
+    profile: 'professor'
+  },
+  {
+    id: 1,
+    name: 'Estudante da Silva Souza',
+    email: 'teste@gmail.com',
+    profile: 'professor'
+  },
+  {
+    id: 1,
+    name: 'Estudante da Silva Souza',
+    email: 'teste@gmail.com',
+    profile: 'professor'
+  },
+  {
+    id: 1,
+    name: 'Estudante da Silva Souza',
+    email: 'teste@gmail.com',
+    profile: 'professor'
+  },
 ]
 
 const headers = ref<InternalDataTableHeader>([
@@ -72,4 +91,11 @@ const headers = ref<InternalDataTableHeader>([
   {title: 'Ação', key: 'action'}
 ]);
 
+function aproveUser(user) {
+  console.log(user)
+}
 </script>
+
+<style>
+
+</style>
