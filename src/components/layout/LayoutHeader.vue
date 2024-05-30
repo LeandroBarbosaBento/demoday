869639ab5a82f1845ff7e3ade406c66355ca17c2
leandro <leandro@pop-os.localdomain>
2024-05-30 14:08:35 -0300

    fix: comportamentos do menu lateral

--- src/components/layout/LayoutHeader.vue
+++ src/components/layout/LayoutHeader.vue
@@ -2,15 +2,24 @@
     <header class="header px-8">
       <div class="header__content">
         <div class="text-white app-font-weight-semibold app-font-size-xl">
+          <v-icon 
+            color="white" 
+            icon="mdi-menu"
+            size="40"
+            @click="$emit('toggleSidebar')"
+            class="mr-5"
+          />
           {{ route.meta.pageTitle }}
         </div>
       </div>
     </header>
 </template>
 <script setup lang="ts">
-const route = useRoute()
-</script>
 
+const route = useRoute()
+defineEmits(['toggleSidebar'])
+
+</script>
 <style scoped lang="scss">
 .header {
   width: 100%;
