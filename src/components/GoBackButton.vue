07a96a6591ed9d124283202e618b0a75f345a59d
leandro <leandro@pop-os.localdomain>
2024-07-08 14:13:27 -0300

    feat: go back button as component

---
+++ src/components/GoBackButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-btn
+        class="py-1 px-1 mt-10 mb-10"
+        @click="$router.go(-1)"
+        icon
+        flat
+    >
+        <v-icon 
+            color="red-ufba" 
+            icon="mdi-arrow-left-thin-circle-outline"
+            size="40"
+        />
+</v-btn>
+</template>
