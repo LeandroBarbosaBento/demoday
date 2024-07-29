6f6fa9ca83cf8bad385a2c91ceda76da311d7bce
Felipe Angelo <fangeloferreira2023@gmail.com>
2024-07-29 10:35:52 -0300

    aplicando interface user e configurando inicio admin

--- src/pages/admin/inicio/index.vue
+++ src/pages/admin/inicio/index.vue
@@ -53,7 +53,50 @@
     </v-col>
     <v-col cols="6">
       <div class="px-4 py-4 elevation-1 rounded-lg ">
-        sdfsdfdfsdf
+        <h1
+          class="app-font-size-xlg app-font-weight-bold text-gray-600"
+        >
+          Usuários Pendentes
+        </h1>
+        <v-data-table class="px-4 py-4 elevation-1 rounded-lg mt-3"
+          :headers="headers"
+          :items="items"
+        >
+          <template v-slot:item.action="{ item }">
+            <v-btn 
+              variant="outlined" 
+              color="blue-ufba" 
+              rounded="lg"
+              @click="handleAproveUser(item)"
+              class="mr-3"
+            >
+              Aprovar
+            </v-btn>
+            <v-btn 
+              variant="outlined" 
+              color="red-ufba" 
+              rounded="lg"
+              @click="handleRejectUser(item)"
+            >
+              Rejeitar
+            </v-btn>
+          </template>
+            
+          <template v-slot:item.type="{ item }">
+            <v-btn
+              class="py-4 cursor-default"
+              block
+              :color="userType[item.type].color" 
+              rounded="lg"
+              size="small"
+              flat
+            >
+              <span class="text-white font-weight-bold"> 
+                {{userType[item.type].text }} 
+              </span> 
+            </v-btn>
+          </template>
+        </v-data-table>
       </div>
     </v-col>
   </div>
@@ -64,8 +107,11 @@
 import { ref, onMounted } from 'vue';
 import axiosInstance from '@/api/axiosInstance';
 import colors from '@/theme/default/index';
-import { Demoday, Project } from '@/types/index';
+import { Demoday, Project, User } from '@/types/index';
 import { typeColorCategory } from '@/theme/default/index'
+
+import type { InternalDataTableHeader } from 'vuetify/lib/components/VDataTable/VDataTable'
+import Swal from 'sweetalert2'
 
 definePageMeta({
   layout: 'default-layout',
@@ -79,7 +125,114 @@ const activeDemoday = ref<Demoday>();
 
 const projectsAccepted = ref<Project []>([]);
 
-async function evaluateProject(id: any) {
+const items = ref<User []>([]);
+
+const userType = {
+  'STUDENT': {color: 'blue-ufba', text: 'estudante'},
+  'ADMIN': {color: 'red-ufba', text: 'administrador'},
+  'PROFESSOR': {color: 'green-ufba', text: 'professor'},
+}
+
+const headers = ref<InternalDataTableHeader>([
+  {title: 'Nome', key: 'name'},
+  {title: 'Email', key:'email'},
+  {title: 'Perfil', key: 'type'},
+  {title: 'Ação', key: 'action'}
+]);
+
+function handleAproveUser(user: User) {
+  Swal.fire({
+    title: `Deseja mesmo aprovar o usuário ${user.name}?`,
+    showDenyButton: true,
+    confirmButtonText: "<span class='text-white'>Aprovar</span>",
+    confirmButtonColor: "green",
+    denyButtonText: "<span class='text-white'>Cancelar</span>"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      aproveUser(user);
+    }
+  }); 
+}
+
+async function aproveUser(user: User) {
+  try {
+    isLoading.value = true;
+    const { data } = await axiosInstance.post('/user/setuserstatus', null, {
+      params: {
+        userId:user.id, 
+        userStatus: 'APPROVED'
+      }
+    });
+
+    Swal.fire({
+      title: "Aprovado",
+      text: "Usuário aprovado com sucesso",
+      icon: "success",
+      showConfirmButton: false,
+      timer: 1250
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    isLoading.value = false;
+    listUsers();
+  }
+}
+
+function handleRejectUser(user: User) {
+  Swal.fire({
+    title: `Deseja mesmo rejeitar o usuário ${user.name}?`,
+    showDenyButton: true,
+    confirmButtonText: "<span class='text-white'>Rejeitar</span>",
+    confirmButtonColor: "red",
+    denyButtonText: "<span class='text-white'>Cancelar</span>",
+    denyButtonColor: "gray",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      rejectUser(user);
+    }
+  });
+}
+
+async function rejectUser(user: User) {
+  try {
+    isLoading.value = true;
+    const { data } = await axiosInstance.post('/user/setuserstatus', null, {
+      params: {
+        userId:user.id, 
+        userStatus: 'REJECTED'
+      }
+    });
+    Swal.fire({
+      title: "Rejeitado",
+      text: "Usuário rejeitado com sucesso!",
+      icon: "success",
+      showConfirmButton: false,
+      timer: 1250
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    isLoading.value = false;
+    listUsers();
+  }
+}
+
+async function listUsers(){
+  try {
+    isLoading.value = true;
+    const { data } = await axiosInstance.get('/user/pending');
+    items.value = data;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+//
+
+async function evaluateProject(id: number) {
   navigateTo(`/student/vote/${id}`);
 }
 
@@ -114,6 +267,7 @@ onMounted(async () => {
       const idDemoday = activeDemoday.value.id
       await getDemodayAcceptedProjects(idDemoday);
   }
+  listUsers();
 });
 </script>
 <style lang="scss" scoped>
